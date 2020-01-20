c58bb224270816b4aad23ee460a3269377acfc17
Sanjana Vijayshankar <vijay092@umn.edu>
2020-01-20 12:35:02 -0600

    using casadi

---
+++ casadi.py
@@ -0,0 +1,24 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 12:02:47 2020
+
+@author: sanja
+"""
+from casadi import *
+
+
+x = SX.sym('x',2); 
+nlp = {'x':x, 'f':x.T @ x, 'g':0}
+S = nlpsol('S', 'ipopt', nlp)
+print(S)
+
+
+x0 = np.array([[1,1],[1,1]])
+
+r = np.zeros(2)
+r = S(x0,\
+      lbg=r, ubg=r)
+x_opt = r['x']
+print('x_opt: ', x_opt)
