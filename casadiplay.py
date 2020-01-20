2a6f8d66fd499ef23352e87015d0898d68f6afcc
Sanjana Vijayshankar <vijay092@umn.edu>
2020-01-20 15:21:35 -0600

    matrix

--- casadiplay.py
+++ casadiplay.py
@@ -9,13 +9,14 @@ Created on Wed Jan 15 12:02:47 2020
 from casadi import *
 
 
-x = SX.sym('x',2); 
-b = [1]
-nlp = {'x':x, 'f':x.T @ x + b, 'g':x}
+x = SX.sym('x',2,2); 
+z = x.T @ x
+f = reshape(x,4,1)
+nlp = {'x':f, 'f':z[0] + z[3], 'g':z[0]}
 S = nlpsol('S', 'ipopt', nlp)
 print(S)
 
 
-r = S(x0=[0,0],lbg=[1,1], ubg=[1,1])
+r = S(x0=[0,1,3,4],lbg=[1], ubg=[1])
 x_opt = r['x']
 print('x_opt: ', x_opt)
