efa78daabe39ee9ccd34449d7ec33f7d630849a9
Sanjana Vijayshankar <vijay092@umn.edu>
2020-01-27 11:43:57 -0600

    lacy

--- SysID.py
+++ SysID.py
@@ -6,3 +6,14 @@ Created on Fri Jan 24 14:03:00 2020
 """
 
 
+'''
+
+This code simulates a system identification problem cast as an LMI. 
+The idea is driven by a paper by Lacy and Berstein
+
+"Subspace identification with guaranteed stability using constrained optimization"
+
+This paper discusses
+
+'''
+
