20798f988e0ffa225d18c8726797aa834982b044
Sanjana Vijayshankar <vijay092@umn.edu>
2020-01-27 12:04:21 -0600

    bleh

--- SysID.py
+++ SysID.py
@@ -13,7 +13,21 @@ The idea is driven by a paper by Lacy and Berstein
 
 "Subspace identification with guaranteed stability using constrained optimization"
 
-This paper discusses
+This paper discusses a stability preserving framework for system identification 
+ of linear systems using LMIs 
+
+Going to implement this using picos 
 
 '''
+
+
+import picos as pic
+import cvxopt as cvx
+import numpy as np
+
+
+
+A = np.array([[1,0],[4,5]])
+
+A_cvx = cvx.matrix(A)
 
