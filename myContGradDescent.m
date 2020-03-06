4a5181c540606e625182b9ae548c47d7a4def889
Sanjana Vijayshankar <vijay092@umn.edu>
2020-03-06 17:01:28 -0600

    correct the gradient

--- myContGradDescent.m
+++ myContGradDescent.m
@@ -2,12 +2,12 @@
 clear all; close all
 n = 2;
 csys = rss(n); 
-ts = .1;
+ts = .4;
 sys = c2d(csys, ts);
 
 
 % Full order system matrices: 
-t = 0:ts:5;
+t = 0:ts:30;
 X = zeros(n,length(t));
 Y = zeros(1,length(t)-1);
 A = sys.A; B = sys.B; C = sys.C; D = sys.D;
@@ -34,7 +34,7 @@ X1 = X(1:n,1:n);
 % because i was having problems with constant step size-- I am working on
 % adaptive step size now. 
 gradFun = @(t,x) gradODE(t,x,X1);
-options = odeset('RelTol',1e-6,'AbsTol',1e-8);
+%options = odeset('RelTol',1e-6,'AbsTol',1e-8);
 [t,x_opt] = ode23(gradFun,t, rand((2*n)^2,1));
 
 % Get back required matrices:
@@ -45,7 +45,7 @@ Q_opt = V_opt(1:n,n+1:end);
 A_opt = (Q_opt * inv(P_opt));
 B_opt = X(:,n+1:end); 
 
-% The output equation:
+% The output equation:6
 Rx = Rinv([X1_til;U1])*blkdiag(P_opt, 1); 
 Y = Y*Rx*Rinv([X1_til;U1]*Rx);
 C_opt = Y(1:n);
@@ -57,8 +57,10 @@ for i = 1:(length(t)-1)
     x_id(:,i+1) = A_opt*x_id(:,i) + B_opt;
     y_id(:,i) = C_opt*x_id(:,i) + D_opt;
 end
-figure(1)
+figure(1);
 hold on;
 plot(y_id,'r*')
+
+V_opt
 
 
